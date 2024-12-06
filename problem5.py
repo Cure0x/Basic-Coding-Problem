9d989483da2b4a4de90ebcb3fff12dafad8d0314
muflih <mufisika13@gmail.com>
2024-12-06 23:50:29 +0700

    First suggest for Problem 5

--- problem5.py
+++ problem5.py
@@ -44,3 +44,29 @@ graph_Jarak ={
 # Jika belum bertemu coba lagi
 # Jika sudah bertemu dengan Node E, simpan semua node yang dilalui dan jumlah weight yang dilewati oleh perjalanan pencarian jalan tersebut ke dalam list (?)
 # Selanjutnya coba di percabangan sebelumnya yang belum dicek
+
+#Karena dalam pencarian jalan kita akan mencari dan melakukan decisiion di yang paling akhir maka menggunakan data structure stack
+# Karena tidak boleh melewati jalan yang sama, maka stack akan menggunakan set
+
+def finding_Route (route, start, finish):
+    stack = []
+    distance = 0
+    node = start
+    if node != finish:
+        path = [node]
+        next_Nodes = list(route[node].keys())
+        print(f"path before update = {path}")
+        print(f"next node = {next_Nodes}")
+        stack += next_Nodes
+        next_Path = stack[-1]
+        distance = route[node][next_Path]
+        node = next_Path
+        path.append(node)
+        print(f"path after update = {path}")
+        print(f"Total Distance = {distance}")
+        print(f"All the stack after update = {stack}")
+        print(f"The node that we are now = {node}")
+
+    pass
+
+finding_Route(graph_Jarak, 'A', 'E')
