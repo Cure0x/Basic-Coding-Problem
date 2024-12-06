de2c0eb54a4036ac76ff9d8ca58ed5f40bc4bf99
muflih <mufisika13@gmail.com>
2024-12-06 16:14:35 +0700

    final answer to problem 2

--- problem2.py
+++ problem2.py
@@ -1,22 +1,35 @@
 import numpy as np
+import math
 
 def degree_Converter (sudut):
     rad = (np.pi/180)*sudut
     return rad
 
 def deret_Mclaurin(iter,x):
-    cos = (-1**iter) * (x**(2*iter))/np.math.factorial(2*iter)
+    cos = ((-1)**iter) * (x**(2*iter)) / math.factorial(2*iter)
     return cos
 
-def error_Value(aprox_Value, true_Value):
+def calculate_Error(aprox_Value, true_Value):
     error = ((aprox_Value-true_Value)/true_Value) * 100
+    error = abs(round(error,3))
     return error
 
-sudut = input(f"\nBesar sudut (derajat) = ")
+sudut = int(input(f"\nBesar sudut (derajat) = "))
 
 sudut_Rad = degree_Converter(sudut)
-
 true_Value = np.cos(sudut_Rad)
 
-aprox_Value = deret_Mclaurin(i,sudut_Rad)
-error_Value = error_Value(true_Value, aprox_Value)
+iter = 0
+aprox_Value = deret_Mclaurin(iter,sudut_Rad)
+error_Value = calculate_Error(true_Value, aprox_Value)
+
+print(f"true Value cos ({sudut} derajat) = {true_Value}")
+
+print (f"\n Jumlah Suku        AV Cosinus      ER Cosinus")
+print(f"{iter}                 {aprox_Value:.6f}          {error_Value:.3f} %")
+
+while error_Value >= 1:
+    iter += 1
+    aprox_Value += deret_Mclaurin(iter,sudut_Rad)
+    error_Value = calculate_Error(true_Value, aprox_Value)
+    print(f"{iter}                 {aprox_Value:.6f}          {error_Value:.3f} %")
